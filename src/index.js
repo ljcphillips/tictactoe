82b4f883b966f1d3573d3025e235e878baee752f
Lorraine Philllips <ljcphillips@gmail.com>
2018-07-10 14:33:42 +0100

    added feature to stop play once game is won

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ let tictactoe = new TicTacToe();
 
 
 $( "td" ).click(function() {
-  if(!$( this ).text()){
+  if(!$( this ).text() && !tictactoe.isWon()){
     $( this ).text(tictactoe.currentPlayer)
     tictactoe.play($(this).attr("id"))
     if(tictactoe.isWon()){
